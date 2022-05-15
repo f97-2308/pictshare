66d19ffe00c2b40659cdb767fe2b01b17fb4590f
Huỳnh Đức Khoản <35532964+f97@users.noreply.github.com>
2022-05-15 08:38:53 +0000

    🎉 `base64()` and `geturl()` with happy case

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ## pictshare
+
+### install
+
+```
+yarn add pictshare
+```
+
+### usage
+
+```js
+const pictshare = new Pictshare('https://www.pictshare.net/')
+const data = await pictshare.geturl('https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI')
+``
+
+### todo
+
+- [x] base64 (base64: string)
+- [x] geturl (url: string) 
+- [ ] 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-import os from 'os';
-export default os;
+import axios from 'axios';
+
+// const uuid = () => (new Date()).getTime().toString(36)
+
+class Pictshare {
+  host: string;
+
+  constructor (host: string) {
+    this.host = host
+  }
+
+  async base64 (base64: string) {
+    const { data } = await axios.post(`${this.host}/api/base64.php`, `base64=data:image/*;base64,${base64}`)
+    if(data.status === 'ok') return data
+  }
+
+  async geturl (url: string) {
+    try {
+      const { data } = await axios(`${this.host}/api/geturl.php?url=${url}`)
+      if(data.status === 'ok') return data
+    } catch (error) {
+      return error
+    }
+  }
+}
+
+export default Pictshare 
